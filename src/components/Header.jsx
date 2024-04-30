4112aa354b617f5eae6d347939743f9494c16a52
Ein Cagle <eincagle@live.com>
2024-04-30 14:41:56 -0500

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header id="header">
+      <img
+        src="src\assets\investment-calculator-logo.png"
+        alt="Logo showing a bag of money"
+      ></img>
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
